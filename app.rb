12bb6ab5b37ff99ebc11430f935bc1f255b88105
jbharwood <josephblakeharwood@gmail.com>
2019-01-28 10:55:02 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Joseph Harwood"
+  end
+
+  get '/hometown' do
+    "My hometown is North Charleston, SC"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Cigarettes and Saints"
+  end
+
 end
