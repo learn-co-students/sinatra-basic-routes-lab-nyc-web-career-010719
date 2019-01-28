ca21e04cba847792f5d01dd4ff7a1555fd82d705
cvolpe12 <christopher.volpe12@gmail.com>
2019-01-28 13:28:06 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Chris"
+  end
+
+  get '/hometown' do
+    "My hometown is Brewster"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Going to California"
+  end
+
 end
