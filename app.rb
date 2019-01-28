ddcfb9bae96c1934452efe5a00fd109d7b413cbf
davidfloyd91 <davidfloyd91@gmail.com>
2019-01-28 12:19:05 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is not."
+  end
+
+  get '/hometown' do
+    "My hometown is not either."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is also not."
+  end
 end
