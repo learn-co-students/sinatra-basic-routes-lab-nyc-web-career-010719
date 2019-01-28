efcabab79cbf633d59dbf577d1dde9f0f7155be4
Elijah Cleveland <elicleveland12@gmail.com>
2019-01-28 10:45:31 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Eli"
+  end
+
+  get '/hometown' do
+    "My hometown is Verona, NY"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Ooh La La by the Faces"
+  end
+
+
+
 end
